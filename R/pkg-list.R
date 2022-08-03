7b52b66e918fd998cd5951a0719fe8b94b3439fd
Chris Muir <cdmuir@hawaii.edu>
2022-08-02 14:29:01 -1000

    Update pkg-list.R

--- R/pkg-list.R
+++ R/pkg-list.R
@@ -1,18 +1,20 @@
-pkg_list <- list(
+pkg_list = list(
   rprofile = c(
     "devtools", "lintr", "pkgdown", "remotes", "reprex", "rmarkdown",
     "roxygen2md", "testthat", "usethis"
   ),
   conflicted = c("conflicted"),
-  parallel = c("doParallel", "foreach", "future"),
-  misc = c("data.table", "units"),
+  parallel = c("doParallel", "foreach", "furrr", "future"),
+  misc = c("checkmate", "data.table", "reticulate", "units"),
   tidyverse_extras = c(
     "blob", "cli", "crayon", "hms", "glue", "lubridate", "magrittr", "pillar",
-    "readxl", "rlang"
+    "purrrlyr", "readxl", "rlang"
   ),
   tidyverse_core = c(
     "ggplot2", "tibble", "tidyr", "readr", "purrr", "dplyr", "stringr",
     "forcats"
   ),
-  plot = c("cowplot", "ggtext", "ggrepel", "grid", "scales", "viridis")
+  plot = c("cowplot", "ggdist", "ggtext", "ggrepel", "grid", "scales", "viridis"),
+  stan = c("brms", "loo" ,"rstan", "rstanarm", "tidybayes"),
+  phylogenetics = c("ape", "phytools")
 )
